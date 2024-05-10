e492610cd75f9132da214a26611098554275f15a
Will <166304230+sorwill@users.noreply.github.com>
2024-05-10 06:21:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const Eris = require("eris");
-const keep_alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js');
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
+
+bot.on("ready", () => {
+  console.log("Bot is ready!");
+
+  // Set the bot's status
+  bot.editStatus("dnd", {
+    name: "slow replies",
+    type: 0 // 0: Playing, 1: Streaming, 2: Listening, 3: Watching
+  });
+});
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
